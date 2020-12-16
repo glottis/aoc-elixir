db64db99d53b45577486d21503a42954923bf819
johan <skin_tiicket@hotmail.com>
2020-12-16 20:03:28 +0100

    day six done

--- lib/202006.ex
+++ lib/202006.ex
@@ -4,11 +4,20 @@ defmodule Aoc202006 do
     File.read!("./input/202006.in")
     |> String.split("\n\n")
     |> Enum.map(fn l -> String.split(l, "\n") end)
+    |> Enum.map(&dedup_list/1)
+  end
+
+
+  def dedup_list(x, new \\ [])
+  def dedup_list([], y), do: y
+  def dedup_list([x | rest], new) do
+    y = String.graphemes(x) |> Enum.uniq() |> Enum.join()
+    dedup_list(rest, [y] ++ new)
   end
 
   def part1 do
     input()
-    |> Enum.map(&conc_split/1)
+    |> Enum.map(&join_split/1)
     |> Enum.map(&Enum.uniq/1)
     |> Enum.map(&Enum.count/1)
     |> Enum.sum()
@@ -20,15 +29,21 @@ defmodule Aoc202006 do
     |> Enum.sum()
   end
 
-  def count_everyone([x]), do: String.length(x) # one element == one person, no need for group freq
-  def count_everyone(x) when is_list(x) do
+  def count_everyone([x]), do: String.length(x) # one element == one person, no need for group count
+  def count_everyone(x) do
     x
-    |> conc_split()
+    |> join_split()
     |> Enum.frequencies
-    |> Enum.count(fn {_key, value} -> value > 1 end)
+    |> Enum.count(fn {_key, value} -> value == length(x) end) # compare against the number ppl in the group
   end
 
-  def conc_split([x]), do: String.graphemes(x)
-  def conc_split([x, y]), do: conc_split([x <> y])
-  def conc_split([x, y | rest]), do: conc_split([x <> y | rest])
+  @doc """
+  Joins all elements and then splits them: eg
+  [aa, ab, ac] => aaabac => [a, a, a, b, a, c]
+  """
+  def join_split(x) do
+    x
+    |> Enum.join()
+    |> String.graphemes()
+  end
 end
