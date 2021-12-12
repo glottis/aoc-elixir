101075f31abdbb1b17dfa595a7483dc826384e8b
glottis <skin_tiicket@hotmail.com>
2021-12-12 22:00:16 +0100

    Day 9 part 2 naive edition

--- lib/202109.ex
+++ lib/202109.ex
@@ -6,56 +6,99 @@ defmodule Aoc202109 do
     |> Enum.map(fn x ->
       x
       |> Enum.map(&String.to_integer/1)
+      |> Enum.with_index()
     end)
+    |> Enum.with_index()
   end
 
   def part1 do
-    {heat_map, maxs} = input() |> create_heat_map()
-
-    check_heat_map(heat_map, maxs) |> Enum.map(&(&1 + 1)) |> Enum.sum()
+    input()
+    |> create_height_map()
+    |> find_smaller_adjacents_loc
+    |> Enum.map(&(&1 + 1))
+    |> Enum.sum()
   end
 
-  defp find_smaller_adjacents_loc(hm, x, y) do
-    curr_pos = hm[{x, y}]
-    neighs = [hm[{x, y - 1}], hm[{x + 1, y}], hm[{x, y + 1}], hm[{x - 1, y}]]
+  def part2 do
+    height_map =
+      input()
+      |> create_height_map()
 
-    smaller =
-      neighs
-      |> Enum.filter(fn x -> !is_nil(x) end)
-      |> Enum.filter(fn x -> x < curr_pos end)
+    found_basins_loc =
+      height_map
+      |> find_smaller_adjacents_loc(true)
+      |> Enum.reduce(%{}, fn {x, y, z}, acc -> Map.put(acc, {x, y}, z) end)
 
-    {curr_pos, smaller}
+    found_basins_loc
+    |> find_downard_wrapper(height_map)
+    |> Enum.sort()
+    |> Enum.reverse()
+    |> Enum.take(3)
+    |> Enum.reduce(fn x, acc -> x * acc end)
   end
 
-  def check_heat_map(_hm, {_max_x, max_y}, low_points, _x, y) when y > max_y, do: low_points
+  def find_downard_wrapper(loc, height_map) do
+    loc |> Enum.map(fn node -> [node] |> find_downward_flows(height_map) |> Enum.count() end)
+  end
 
-  def check_heat_map(hm, {max_x, max_y}, low_points, x, y) when max_x == x,
-    do: check_heat_map(hm, {max_x, max_y}, low_points, 0, y + 1)
+  def find_downward_flows([], _height_map, results), do: results
 
-  def check_heat_map(hm, maxs, low_points \\ [], x \\ 0, y \\ 0) do
-    {curr_pos, smaller} = find_smaller_adjacents_loc(hm, x, y)
+  def find_downward_flows([{{x, y}, k} = current_node | t], height_map, results \\ %{}) do
+    case results[current_node] do
+      nil ->
+        entries =
+          [
+            {{x, y - 1}, height_map[{x, y - 1}]},
+            {{x + 1, y}, height_map[{x + 1, y}]},
+            {{x, y + 1}, height_map[{x, y + 1}]},
+            {{x - 1, y}, height_map[{x - 1, y}]}
+          ]
+          |> Enum.filter(fn {{_q, _w}, e} -> !is_nil(e) && e > k && e != 9 end)
+          |> Enum.filter(fn {{q, w}, _e} -> is_nil(results[{q, w}]) end)
 
-    case length(smaller) do
-      0 ->
-        check_heat_map(
-          hm,
-          maxs,
-          [curr_pos] ++ low_points,
-          x + 1,
-          y
-        )
+        added_entries =
+          ([current_node] ++ entries)
+          |> Enum.reduce(results, fn {{q, w}, e}, acc -> Map.put(acc, {q, w}, e) end)
+
+        find_downward_flows(entries ++ t, height_map, added_entries)
 
       _ ->
-        check_heat_map(hm, maxs, low_points, x + 1, y)
+        find_downward_flows(t, height_map, results)
     end
   end
 
-  def create_heat_map([[] | []], heat_map, x, y),
-    do: {heat_map, {x, y}}
+  def find_basins(basins, h_m) do
+  end
 
-  def create_heat_map([[] | t], heat_map, _x, y), do: create_heat_map(t, heat_map, 0, y + 1)
+  def find_smaller_adjacents_loc(h_m, cordinates_only \\ false) do
+    h_m
+    |> Enum.reduce([], fn {{x, y}, k}, acc ->
+      neigh =
+        [h_m[{x, y - 1}], h_m[{x + 1, y}], h_m[{x, y + 1}], h_m[{x - 1, y}]]
+        |> Enum.filter(fn x -> !is_nil(x) end)
+        |> Enum.all?(&(&1 > k))
 
-  def create_heat_map([[h | tt] | t], heat_map \\ %{}, x \\ 0, y \\ 0) do
-    create_heat_map([tt | t], Map.put_new(heat_map, {x, y}, h), x + 1, y)
+      case cordinates_only do
+        true ->
+          case neigh do
+            true -> [{x, y, h_m[{x, y}]}] ++ acc
+            _ -> acc
+          end
+
+        false ->
+          case neigh do
+            true -> [k] ++ acc
+            _ -> acc
+          end
+      end
+    end)
+  end
+
+  def create_height_map([], height_map), do: height_map
+  def create_height_map([{[], _y} | t], height_map), do: create_height_map(t, height_map)
+
+  def create_height_map([{h, y} | t], height_map \\ %{}) do
+    [{k, x} | tt] = h
+    create_height_map([{tt, y} | t], Map.put_new(height_map, {x, y}, k))
   end
 end
